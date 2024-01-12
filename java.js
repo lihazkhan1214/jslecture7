292232ee84b7b1dc9cd14b63bec4806b9669b056
lihazkhan1214 <lehazali123@gmail.com>
2024-01-12 16:33:20 +0500

    add project

---
+++ java.js
@@ -0,0 +1,27 @@
+// let txt="helloworld";
+
+// let b=txt.charAt(9)
+
+// let b=txt.charCodeAt(0) return  ascii code
+
+// let b=txt.charAt(2); return position of character 
+// console.log(b)
+
+// let txt="Helloworld1*";
+// let b=txt[4];
+// let b=txt.slice(-8,-1);
+
+// let b=txt.substring(-2,7);
+// let b=txt.toUpperCase();
+// let b=txt.toLowerCase();
+// console.log(b);
+
+let firstname="Awan";
+
+let middlename ="Ali";
+let lastname="shah";
+
+// let fullname=firstname+ " " +lastname;
+
+let fullname=firstname.concat(" ",middlename," ",lastname);
+console.log(fullname);
